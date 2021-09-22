4119d250127d6ccb5b9e015dd8dd62a1107ac341
Beenzu <a.b.mweemba@outlook.com>
2021-09-22 06:34:22 +0000

    Final Commit

--- main.js
+++ main.js
@@ -13,8 +13,30 @@ let weather = {
     const { temp, humidity} = data.main
     const {speed} = data.wind
 
-    console.log(name, icon, description, temp, humidity, speed)
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    document.querySelector(".temp").innerText = `${temp}°C`
+    document.querySelector(".description").innerText = description
+    document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`
+    document.querySelector(".wind").innerText = `Wind speed: ${speed}km/h`
+    document.querySelector(".search-container")
+    document.querySelector(".weather").classList.remove("loading")
+    document.body.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?${name}")`
+   },
+   search: function(){
+       this.fetchWeather(document.querySelector(".search-bar").value)
    }
 
 }
 
+document.querySelector(".search-container button").addEventListener("click", function(){
+    weather.search()
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function(e){
+    if(e.key == "Enter"){
+        weather.search()
+    }
+})
+
+weather.fetchWeather("Casablanca")
