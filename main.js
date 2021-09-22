75eda7a2741d742ae53f756407fd3c309283a199
Beenzu <a.b.mweemba@outlook.com>
2021-09-22 05:15:20 +0000

    API implement

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-GIT 
+let weather = {
+   
+   apiKey: "1e103184852ca9667847334e6084fc4b",
+   fetchWeather: function(city){
+       fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey + "&units=metric")
+       .then(response => response.json())
+       .then(data => this.displayWeather(data))
+       
+   },
+   displayWeather: function(data) {
+    const {name} = data
+    const {icon, description} = data.weather[0]
+    const { temp, humidity} = data.main
+    const {speed} = data.wind
+
+    console.log(name, icon, description, temp, humidity, speed)
+   }
+
+}
+
